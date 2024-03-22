19a9cc5685f62cbe696c0e1b48c523466bac7d98
dmontenegroh <dmontenegro.top@gmail.com>
2024-03-22 12:31:31 -0300

    Add Drag And Drop del repo react-dnd con una nueva version del Ordenar Drop

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { DragDropContext } from "@hello-pangea/dnd";
+
 import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import TodoComputed from "./components/TodoComputed";
@@ -6,6 +8,14 @@ import TodoFilter from "./components/TodoFilter";
 import TodoList from "./components/TodoList";
 
 const initialStateTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const reorder = (list, starIndex, endIndex) => {
+  const result = [...list];
+  const [removed] = result.splice(starIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
 
 const App = () => {
   const [todos, setTodos] = useState(initialStateTodos);
@@ -59,18 +69,34 @@ const App = () => {
     localStorage.setItem("todos", JSON.stringify(todos));
   }, [todos]);
 
+  const handleDragEnd = (result) => {
+    const { destination, source } = result;
+
+    if (!destination) return;
+
+    if (
+      source.index === destination.index &&
+      source.droppableId === destination.droppableId
+    )
+      return;
+
+    setTodos((prevTask) => reorder(prevTask, source.index, destination.index));
+  };
+
   return (
-    <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat transition-all duration-1000 md:bg-[url('./assets/images/bg-desktop-light.jpg')] dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] ">
+    <div className="min-h-screen bg-gray-200 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat transition-all duration-1000 dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')] ">
       <Header />
 
       <main className="container mx-auto mt-8 px-4 md:max-w-xl">
         <TodoCreate createtodo={createtodo} />
 
-        <TodoList
-          todos={filteredTodos()}
-          removeTodo={removeTodo}
-          updateTodo={updateTodo}
-        />
+        <DragDropContext onDragEnd={handleDragEnd}>
+          <TodoList
+            todos={filteredTodos()}
+            removeTodo={removeTodo}
+            updateTodo={updateTodo}
+          />
+        </DragDropContext>
 
         <TodoComputed
           todos={filteredTodos()}
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,34 @@
+import { Droppable, Draggable } from "@hello-pangea/dnd";
+
 import TodoItem from "./TodoItem";
 
 const TodoList = ({ todos, removeTodo, updateTodo }) => {
   return (
-    <div className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          removeTodo={removeTodo}
-          updateTodo={updateTodo}
-        />
-      ))}
-    </div>
+    <Droppable droppableId={"todos"}>
+      {(droppableProvided) => (
+        <div
+          ref={droppableProvided.innerRef}
+          {...droppableProvided.droppableProps}
+          className="mt-8 overflow-hidden rounded-t-md bg-white transition-all duration-1000 dark:bg-gray-800 [&>article]:p-4"
+        >
+          {todos.map((todo, index) => (
+            <Draggable key={todo.id} index={index} draggableId={`${todo.id}`}>
+              {(draggableProvider) => (
+                <TodoItem
+                  todo={todo}
+                  removeTodo={removeTodo}
+                  updateTodo={updateTodo}
+                  ref={draggableProvider.innerRef}
+                  {...draggableProvider.dragHandleProps}
+                  {...draggableProvider.draggableProps}
+                />
+              )}
+            </Draggable>
+          ))}
+          {droppableProvided.placeholder}
+        </div>
+      )}
+    </Droppable>
   );
 };
 
